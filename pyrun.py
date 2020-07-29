3597c3ef1da6b16f6044f12b48663e2d3e1ab5e9
INE-01 <INE-01@Admins-iMac-5.local>
2020-07-29 19:04:28 +0700

    commit5

--- pyrun.py
+++ pyrun.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column,Integer,String,Date
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:DECecn76676@10.100.2.27:5432/testdb'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:DECecn76676@10.100.2.26:5432/testdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
